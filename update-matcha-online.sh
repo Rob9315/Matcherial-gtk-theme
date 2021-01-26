a34edcff1176600a228e8036fbed88fe2022f85b
Rob9315 <dev.robk@gmail.com>
2021-01-26 23:35:25 +0100

    change some update stuff (won't work yet)

--- update-matcha-online.sh
+++ update-matcha-online.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
-notify-send "Matcha Theme" "Getting the latest version of the Matcha theme..." -i system-software-update
+notify-send "Matcherial Theme" "Getting the latest version of the Matcherial theme..." -i system-software-update
 cd /tmp/; rm -Rf /tmp/matcha.zip 2>/dev/null
 rm -Rf /tmp/matcha-master/ 2>/dev/null
-wget https://github.com/vinceliuice/matcha/archive/master.zip -O matcha.zip
+wget https://github.com/Rob9315/Matcherial-gtk-theme/archive/master.zip -O matcha.zip
 unzip matcha.zip; cd matcha-master
 ./install.sh
-notify-send "All done!" "Enjoy the latest version of Matcha!" -i face-smile
+notify-send "All done!" "Enjoy the latest version of Matcherial!" -i face-smile
